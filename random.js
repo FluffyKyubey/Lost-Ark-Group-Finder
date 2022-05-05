4bac09dcf27536de369b23e971b1c7c0b2e6f2b7
Robert Routhier <robrouth92@gmail.com>
2022-05-05 12:38:12 -0700

    lunch tinker

--- random.js
+++ random.js
@@ -7,7 +7,7 @@ fetch(
       console.log(copy)
       
       
-      
+      //testing shuffle of names, not for final block
       var shuffle = copy.sort(() => Math.random() - 0.5);
       
       for (var i = 0; i < shuffle.length; i++){
@@ -15,40 +15,76 @@ fetch(
         console.log(toons)
       }
       
+      //var randomGroupFour = 4
+      //var randomGroupEight = 8
+      //var groupSelect = document.querySelector('number').textContent
     });
-    //var randomGroupFour = 4
-    //var randomGroupEight = 8
-    //var groupSelect = document.querySelector('number').textContent
-    
-    
-    function chooseGroupSize(){
-      choice = []
-      groupSize = parseInt(modal)
+      
+      function shuffle(data){
+        var currentIndex = data.length, randomIndex;
 
-      if (isNaN(groupSize)){
-        return false;
+        while (currentIndex !==0){
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+
+          [data[currentIndex], data[randomIndex]] = [data[randomIndex], data[currentIndex]];
+        }
       }
-      return true;
+    
+    document.querySelector('.number').addEventListener('keyup', function(event){
+      var number = document.querySelector('.number').value
+      if(event.key == 'Enter'){
+      chooseGroupSize(number)
+      createGroups()
+      printGroups()
+      }
+    })
 
-    }
 
-    function createGroups(){
 
-      var group = [];
-      for (var i = 0; i < groupSize)
+      function chooseGroupSize(){
+        choice = document.querySelector('.number').value
+        groupSize = parseInt(choice)
+        console.log(choice)
 
-    }
-
-    function printGroups(){
-      var newGroup = chooseGroupSize();
-      var groupMembers = document.querySelector('.group')
-
-      if (newGroup){
-
-        var createdGroup = createGroups();
-        groupMembers.value = createdGroup
-      } else {
-        groupMembers.value = ""
+        
+        if (isNaN(groupSize)){
+          return false;
+        }
+        return true;
+        
+      }
+      
+      
+     
+      
+      function createGroups(groups, quantity){
+        
+        var group = [];
+        var shuffleResult = shuffle(groups)
+        for (var i = 0; i < quantity; i++){
+          group.push(shuffleResult[i])
+        }
+        return group
+        
       }
 
-    }
+      var currentQuantity = chooseGroupSize()
+
+      
+
+      var result = createGroups(data, currentQuantity)
+      
+      function printGroups(){
+        var newGroup = chooseGroupSize();
+        var groupMembers = document.querySelector('.group')
+        
+        if (newGroup){
+          
+          var createdGroup = createGroups();
+          groupMembers.value = createdGroup
+        } else {
+          groupMembers.value = ""
+        }
+        
+      }
