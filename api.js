0c38c434cdd05509ba307315e120eadfd405c3e0
John Hull <john.alex.hull@gmail.com>
2022-05-05 21:36:38 -0700

    merged

--- api.js
+++
@@ -1,9 +0,0 @@
-fetch(
-    "https://opensheet.elk.sh/1F77UjfkB9J9qFiH1rwh45uUantZE1V3F02OaCjJIkY8/PlayerInfo"
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((data) => {
-        console.log(data.Character)
-      });
-    });
